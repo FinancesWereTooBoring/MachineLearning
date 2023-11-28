e8e5168171103399e65e90c400963b2cb55e5a84
FinanceWereTooBoring <bohdan.markiv18@gmail.com>
2023-11-28 16:46:34 +0100

    - added more to recipe

--- LassoModel.R
+++ LassoModel.R
@@ -1,5 +1,14 @@
 library(tidymodels)
 library(tidyverse)
 
-lasso_reg <- linear_reg(penalty = tune(), mixture = 1) %>%  
-  set_engine("glmnet")
+linear_reg_recipe <- 
+  recipe(Balance ~ ., data = Credit_train) |> 
+  step_dummy(all_nominal_predictors()) |> 
+  # I still include interaction term
+  step_interact(~ all_predictors():all_predictors()) |> 
+  # remove any resulting variables that have only one value
+  # and thus zero variance ("zv")
+  step_zv(all_predictors()) |> 
+  # normalize the predictors to have mean 0 and SD 1
+  step_normalize(all_predictors())
+
