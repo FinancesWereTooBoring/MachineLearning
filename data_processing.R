f6d1fecbdf9525ff1f3d8349d94369c073fa4647
FinanceWereTooBoring <bohdan.markiv18@gmail.com>
2023-12-05 17:54:14 +0100

    -censoring of 2022

--- data_processing.R
+++ data_processing.R
@@ -12,9 +12,10 @@ final_training <- training(final_training_prediction_split)
 
 analysis_assessment_split <-
   offers |>
+  censor_post_prediction_responses(years = 2022)|>
+  drop_post_prediction_offers()|>
   filter(AppYear <= 2022) |>
   make_appyear_split(test_year = 2022)
 
 analysis_train <- training(analysis_assessment_split)
 assessment_test <- testing(analysis_assessment_split)
-
