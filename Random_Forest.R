efcf8a4877f7a4064b2ee1c381552f4fe61198ac
nadyahagen <78555988+nadyahagen@users.noreply.github.com>
2023-12-07 22:44:47 +0100

    - Developing Random Forest Model (Issue w/ NaN)

--- Random_Forest.R
+++ Random_Forest.R
@@ -1,1 +1,67 @@
-print("Hi bitchess")
+library(tidymodels)
+library(tidyverse)
+library(skimr)
+
+set.seed(221102)
+
+#cv_folds <-
+#  final_training_prediction_split |>
+#  vfold_cv(v = 10, strata = Status)
+
+skim(final_training_prediction_split)
+
+analysis_set_train <- training(final_training_prediction_split)
+assessment_set_test <- testing(final_training_prediction_split)
+
+# CV folds
+cv_folds <-
+  analysis_set_train |>
+  vfold_cv(v = 10, strata = Status)
+  
+skim(analysis_set_train)
+
+# Defining the random forest model
+rf_recipe_downsample <-
+  recipe(Status ~., data = analysis_set_train) #|>
+  #themsis::step_downsample(Status)
+# need to double check if this is necessary
+
+rf_recipe_downsample
+
+# Define the random forest model
+rf_model_tune <-
+  rand_forest(mtry = tune(), trees = 1000) |>
+  set_mode("classification") |>
+  set_engine("ranger", importance = "permutation")
+
+rf_tune_wf <- 
+  workflow() |>
+  add_recipe(rf_recipe_downsample) |>
+  add_model(rf_model_tune)
+
+class_metrics <- metric_set(
+  accuracy, kap, sensitivity,
+  specificity, roc_auc
+)
+
+# Tuning the model
+rf_tune_grid <- grid_regular(mtry(range = c(1, 17)), levels = 17)
+
+num_cores <- parallel::detectCores()
+num_cores
+
+doParallel::registerDoParallel(cores = num_cores - 1L)
+
+set.seed(231164)
+rf_tune_res <- tune_grid(
+  rf_tune_wf,
+  resamples = cv_folds,
+  grid = rf_tune_grid,
+  metrics = class_metrics
+)
+
+# Unable to do randomforest with missing data in ResponseDate
+
+# Plotting results for metrics
+
+
